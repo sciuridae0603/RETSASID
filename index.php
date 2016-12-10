ccd31ace9dc242e65fb8d2196f3f70429f6082bb
root <root@ubuntu.members.linode.com>
2016-12-10 18:42:11 +0000

    Hello World

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-安安你好
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="utf-8">
+    <title>三鐵機場最新消息整合系統</title>
+
+    <!-- 引入 Bootstrap 的 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- main.css -->
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+    <!-- 在 Body 關閉前載入 JS -->
+    <!-- jQuery -->
+    <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <!-- Bootstrap 的 JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> 
+    <!-- Vue.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.3/vue.min.js"></script>
+
+    <!-- main.js -->
+    <script src="main.js"></script>
+</body>
+</html>
+<!-- by 只是路過別人的畢業專題展覽沒想到居然就開始了黑客松 -->
---
+++ main.js
@@ -0,0 +1,32 @@
+(function() {
+  "use strict";
+
+
+  //------ 公告
+  var railway = new Vue({    //Railway
+    el: '#railway',
+    data: {
+      announcements: []
+    }
+  });
+  var hsrailway = new Vue({ //High speed railway
+    el: '#hsrailway',
+    data: {
+      announcements: []
+    }
+  });
+  var tyap = new Vue({      //Taoyuan Airport
+    el: '#tyap',
+    data: {
+      announcements: []
+    }
+  });
+  var ssap = new Vue({      //Songshan Airport
+    el: '#ssap',
+    data: {
+      announcements: []
+    }
+  });
+
+})();
+
